1b8fe99bf1bc82e6c00c0510205d785f3eb11fe1
404874-RearteArizaCesarRafael <404874@tecnicatura.frc.utn.edu.ar>
2025-05-22 20:15:50 -0300

    terminado

--- src/main/java/org/example/parking/Ticket.java
+++ src/main/java/org/example/parking/Ticket.java
@@ -31,13 +31,33 @@ public class Ticket {
     }
 
     public double calcularPrecio() {
-        // TODO implementar el metodo para calcular el importe a abonar segun el tipo de vehiculo
+        // TODO---- implementar el metodo para calcular el importe a abonar segun el tipo de vehiculo
         // AUTO -> 100, SUV -> 130, PICKUP -> 180
         // el importe es por hora redondeando el tiempo hacia arriba,
         // por ejemplo si estuvo 45 minutos se le tarifa por 60, si estuvo 80 minutos se le tarifa por 120 minutos, etc...
         // retornar el importe final
 
-        return 0;
+        double tarifa = 0;
+        long minutos = calcularMinutos();
+
+        long horas = minutos / 60;
+        if (minutos % 60 > 0 ){
+            horas = horas +1;
+        }
+
+        if (vehiculo.getTipo() == Vehiculo.Tipo.AUTO){
+            tarifa = 100;
+        }
+        else if (vehiculo.getTipo() == Vehiculo.Tipo.SUV){
+            tarifa = 130;
+        }
+        else if (vehiculo.getTipo() == Vehiculo.Tipo.PICKUP){
+            tarifa = 180;
+        }
+
+        double precioapagar = tarifa * horas;
+
+        return precioapagar;
     }
 
 }
--- src/test/java/org/example/parking/EstacionamientoTest.java
+++ src/test/java/org/example/parking/EstacionamientoTest.java
@@ -4,17 +4,42 @@ import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 public class EstacionamientoTest {
 
     @Test
     public void testRetirarVehiculo() throws Exception {
-        //TODO test
+        //TODO---- test
+        Estacionamiento estacionamiento = new Estacionamiento();
+        Vehiculo auto = new Vehiculo("af904sn","cronos", Vehiculo.Tipo.AUTO);
+
+        estacionamiento.ingresarVehiculo("123456", "rafa rearte", auto);
+
+        Ticket ticket = estacionamiento.retirarVehiculo("af904sn");
+        assertNotNull(ticket);
+        assertEquals("af904sn", ticket.getVehiculo().getPatente());
     }
 
     @Test
     public void testCalcularPrecio() throws Exception {
-        // TODO test
+        // TODO---- test
+        Cliente cliente = new Cliente("123456", "rafa rearte");
+        Vehiculo auto = new Vehiculo("af904sn","cronos", Vehiculo.Tipo.AUTO);
+        Vehiculo suv = new Vehiculo("ag999fh", "tiguan", Vehiculo.Tipo.SUV);
+        Vehiculo chata = new Vehiculo("onj284","ranger", Vehiculo.Tipo.PICKUP);
+
+        Ticket ticket1 = new Ticket(cliente, auto);
+        Ticket ticket2 = new Ticket(cliente, suv);
+        Ticket ticket3 = new Ticket(cliente, chata);
+
+        ticket1.setHoraSalida(ticket1.getHoraEntrada().plusMinutes(60));
+        ticket2.setHoraSalida(ticket2.getHoraEntrada().plusMinutes(60));
+        ticket3.setHoraSalida(ticket3.getHoraEntrada().plusMinutes(60));
+
+        assertEquals(100.0, ticket1.calcularPrecio());
+        assertEquals(130.0, ticket2.calcularPrecio());
+        assertEquals(180.0, ticket3.calcularPrecio());
     }
 
 }
